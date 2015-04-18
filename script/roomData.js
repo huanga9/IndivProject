3a599fe00efbe8df02ae8b0f8b69f114b17229da
huanga9 <huanga9@tcnj.edu>
2015-04-18 15:36:09 -0400

    Create roomData.js

---
+++ script/roomData.js
@@ -0,0 +1,34 @@
+var userInformation = [
+{
+    "name": "Angela Huang",
+    "id": 81920,
+    "lastName": "Huang",
+    "firstName": "Angela",
+    "building": "Eickhoff Hall",
+    "roomNumber": "231"
+},
+{
+    "name": "Dylan Wulf",
+    "id": 81991,
+    "lastName": "Wulf",
+    "firstName": "Dylan",
+    "building": "Cromwell",
+    "roomNumber": "311"
+},
+{
+    "name": "Graham Roberts",
+    "id": 79550,
+    "lastName": "Roberts",
+    "firstName": "Graham",
+    "building": "Townhouse East",
+    "roomNumber": "19"
+},
+{
+    "name": "Trevor Fullman",
+    "id": 32627,
+    "lastName": "Fullman",
+    "firstName": "Trevor",
+    "building": "Townhouse East",
+    "roomNumber": "23"
+}
+];
