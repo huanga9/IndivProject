e81f158483fc957a058eb9b56ee3ec5e82a02f69
huanga9 <huanga9@tcnj.edu>
2015-04-18 19:42:56 -0400

    Create validateLogin.js

---
+++ script/validateLogin.js
@@ -0,0 +1,25 @@
+function login() {
+
+    // get reference to user input
+    var inputUsername = document.getElementById('username');
+	var inputPassword = document.getElementByID('password');
+	
+	var valid = false;
+	
+	for (var i=0; i <users.length; i++) {
+		if ((inputUsername == users[i].userName) && (inputPassword == users[i].password)) {
+			valid = true;
+			break;
+		}
+	}
+	
+	if (valid) {
+		alert ("Login was successful");
+		window.location = "rankChores.html";
+		return false;
+	}
+	
+	else {
+		alert ("You entered an incorrect username or password. Hint: Username=last name followed by your first initial. Password='se2015'.");
+	}
+}
