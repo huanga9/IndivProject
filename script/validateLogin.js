9e49a11db5bc18fff1a9cc1a899cb85e632bd444
huanga9 <huanga9@tcnj.edu>
2015-04-19 01:39:08 -0400

    testing

--- script/validateLogin.js
+++ script/validateLogin.js
@@ -15,7 +15,7 @@ function login() {
 	
 	if (valid) {
 		alert ("Login was successful");
-		window.location.replace("../View/rankChores.html");
+		 document.location.href = '../View/rankChores.html';
 		return false;
 	}
 	
