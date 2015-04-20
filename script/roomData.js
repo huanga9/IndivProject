1705195794883edfd92df800c8bf82b177fec5dd
Angela Huang <huanga9@tcnj.edu>
2015-04-20 19:46:37 -0400

    Update roomData.js

--- script/roomData.js
+++ script/roomData.js
@@ -6,37 +6,7 @@ var users = [
     "firstName": "Angela",
     "building": "Eickhoff Hall",
     "roomNumber": "231",
-    "userName": "huanga",
-    "password": "se2015"
-},
-{
-    "name": "Dylan Wulf",
-    "id": 81991,
-    "lastName": "Wulf",
-    "firstName": "Dylan",
-    "building": "Cromwell",
-    "roomNumber": "311",
-    "userName": "wulfd",
-    "password": "se2015"
-},
-{
-    "name": "Graham Roberts",
-    "id": 79550,
-    "lastName": "Roberts",
-    "firstName": "Graham",
-    "building": "Townhouse East",
-    "roomNumber": "19",
-    "userName": "robertsg",
-    "password": "se2015"
-},
-{
-    "name": "Trevor Fullman",
-    "id": 32627,
-    "lastName": "Fullman",
-    "firstName": "Trevor",
-    "building": "Townhouse East",
-    "roomNumber": "23",
-    "userName": "fullmant",
+    "userName": "user1",
     "password": "se2015"
 }
 ];
