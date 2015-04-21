2bd24334199a049110ce3759f55bc11d87125918
Angela Huang <huanga9@tcnj.edu>
2015-04-20 21:40:33 -0400

    input validateAccount function

--- script/validateLogin.js
+++ script/validateLogin.js
@@ -20,6 +20,28 @@ function login() {
 	}
 	
 	else {
-		alert ("You entered an incorrect username or password. Hint: Username=last name followed by your first initial. Password='se2015'.");
+		alert ("You entered an incorrect username or password. Hint: Username='user1'. Password='se2015'.");
 	}
 }
+
+function validateAccount () {
+	
+	// gets references to user input
+	var inputFullName = document.getElementById('name').value;
+	var inputBuilding= document.getElementById('building').value;
+	var inputUsername = document.getElementById('username').value;
+	var inputPassword = document.getElementById('password').value;
+
+        var letters = /^[A-Za-z]+$/;  
+        var lettersNumbersUnderscore = /^[a-zA-Z0-9_]+$/;
+        if ( !(inputFullName.value.match(letters)) || !(inputBuilding.value.match(letters)) || !(inputUsername.match(lettersNumbersUnderscore)) || !(ivar inputPassword.match(lettersNumbersUnderscore))) {
+      		alert ("Invalid input!  Note: Name and Building should only contain letters. Username and Password should only contain letters, numbers, and underscores.");
+	}
+	
+	else {
+	
+      		alert('Account successfully created.');  
+     		document.location.href = '../View/rankChores.html'
+      }  
+}
+
