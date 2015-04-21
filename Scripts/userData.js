fb637a7a642796c8642cdd99618941303d3da3da
Angela Huang <huanga9@tcnj.edu>
2015-04-21 13:31:28 -0400

    Create userData.js

---
+++ Scripts/userData.js
@@ -0,0 +1,12 @@
+var users = [
+{
+    "name": "Angela Huang",
+    "id": 81920,
+    "lastName": "Huang",
+    "firstName": "Angela",
+    "building": "Eickhoff Hall",
+    "roomNumber": "231",
+    "userName": "user1",
+    "password": "se2015"
+}
+];
