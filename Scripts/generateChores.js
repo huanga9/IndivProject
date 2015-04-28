7b43e32c16ac2a23eaa437d12880d1eb782e9cee
Angela Huang <huanga9@tcnj.edu>
2015-04-28 19:46:21 -0400

    edited partition algorithm

--- Scripts/generateChores.js
+++ Scripts/generateChores.js
@@ -23,64 +23,64 @@ function assignChores(){
 	if (chore1.length > 0) {
 		chore1Val = parseInt(chore1.val(), 10);
     		values.push(chore1Val);
-    		maptoName.push(chore1.attr('name'));
+    		maptoName.push(chore1.attr('class'));
 	 }
     
 	var chore2=$("input:radio[name='rank2']:checked")
 	if (chore2.length > 0) {
 		chore2Val = parseInt(chore2.val(), 10);
     		values.push(chore2Val);
-   	    	maptoName.push($(this).attr('name'));
+   	    	maptoName.push(chore2.attr('class'));
 	 }
     
 	var chore3=$("input:radio[name='rank3']:checked")
 	if (chore3.length > 0) {
 		chore3Val = parseInt(chore3.val(), 10);
     		values.push(chore3Val);
-   		maptoName.push($(this).attr('name'));
+   		maptoName.push(chore3.attr('class'));
 	 }
         
     	var chore4=$("input:radio[name='rank4']:checked")
 	if (chore4.length > 0) {
 		chore4Val = parseInt(chore4.val(), 10);
     		values.push(chore4Val);
-   		maptoName.push(chore4.name)
+   		maptoName.push(chore4.attr('class'));
 	 }
     
     	var chore5=$("input:radio[name='rank5']:checked")
 	if (chore5.length > 0) {
 		chore5Val = parseInt(chore5.val(), 10);
     		values.push(chore5Val);
-   	  	maptoName.push(chore5.name)
+   	  	maptoName.push(chore5.attr('class'));
 	 }
     
 	var chore6=$("input:radio[name='rank6']:checked")
  	if (chore6.length > 0) {
 		chore6Val = parseInt(chore6.val(), 10);
     		values.push(chore6Val);
-   		maptoName.push(chore6.name)
+   		maptoName.push(chore6.attr('class'));
 	}
         
 	var chore7=$("input:radio[name='rank7']:checked")
 	if (chore7.length > 0) {
 		chore7Val = parseInt(chore7.val(), 10);
     		values.push(chore7Val);
-   		maptoName.push(chore7.name)
+   		maptoName.push(chore7.attr('class'));
 	}
         
     	var chore8=$("input:radio[name='rank8']:checked")
 	if (chore8.length > 0) {
 		chore8Val = parseInt(chore8.val(), 10);
     		values.push(chore8Val);
-   		maptoName.push(chore8.name)
+   		maptoName.push(chore8.attr('class'));
 	 }
         
     	var chore9=$("input:radio[name='rank9']:checked")
 	if (chore9.length > 0) {
 		chore9Val = parseInt(chore9.val(), 10);
     		values.push(chore9Val);
-   		maptoName.push(chore9.name)
-	  }
+   		maptoName.push(chore9.attr('class'));
+	 }
 
 //	 alert ("Thank you for submitting your Rankings.");
 //	 document.location.href = '../View/viewChores.html'
@@ -152,16 +152,16 @@ function greedyPartition(sortedArray, sortedArray2) {
 		names1.push(sortedArray2[2])
 	}
 	
-	for (var i = 2; i<sortedArray.length; i++) {
+	for (var i = 3; i<sortedArray.length; i++) {
 		
 		var sum1 = 0; 
 		for(var j = 0; j < chores1.length; j++) {
-    			sum1 = sum1 + chores1[j];
+    			sum1+=chores1[j];
 		}
 	
 		var sum2 = 0; 
 		for(var k = 0; k < chores2.length; k++) {
-    			sum2 = sum2 + chores2[k];
+    			sum2+=chores2[k];
 		}
 	
 		if (sum1 > sum2) {
