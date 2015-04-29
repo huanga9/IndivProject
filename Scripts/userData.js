198789c7fd94182cced9767b9619469307c5504d
Angela Huang <huanga9@tcnj.edu>
2015-04-28 23:48:59 -0400

    Updated Documentation

--- Scripts/userData.js
+++ Scripts/userData.js
@@ -1,3 +1,18 @@
+/*
+ * Name: Angela Huang
+ * Course: CSC 415 (Software Engineering)
+ * Semester: Spring 2015
+ * Instructor: Dr. Pulimood
+ *
+ * Project Name: The Roommate Application 
+ * Description: a website that allows roommates to track living obligations via chore assignements and communicate through a notfications and feedback page
+ *
+ * Filename: userData.js
+ * Description: This file stores data for the users of the system and is used in the validateAccount.js file.  
+ *
+ * Last Modified: April 28, 2015
+ *
+ */
 var users = [
 {
     "name": "Angela Huang",
